3a43c2d0354ffe2e266aef1fcd7d8c7486d2766c
rafifahrezi75 <110459675+rafifahrezi75@users.noreply.github.com>
2023-01-03 11:45:21 +0700

    Add files via upload

---
+++ javascript 1-12/js/object.js
@@ -0,0 +1,22 @@
+let objek = {
+
+    nama : "smk revit",
+    telp : 123456789,
+
+    buah : ["apel", "jeruk", "mangga"],
+
+    coba : function () {
+        return "coba function dalam object";
+    },
+
+    boleh : true,
+    "tulis aja" : 123456,
+
+};
+
+console.log(objek. nama);
+console.log(objek. telp);
+console.log(objek. boleh);
+console.log(objek. coba());
+console.log(objek. buah[1]);
+console.log(objek["tulis aja"]);
