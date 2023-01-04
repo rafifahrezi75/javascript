dfce8a574175f0aa3d667290f6dd22af15f730c1
rafifahrezi75 <110459675+rafifahrezi75@users.noreply.github.com>
2023-01-04 16:23:14 +0700

    Add files via upload

---
+++ javascript 13-21/js/logika.js
@@ -0,0 +1,36 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+// Or
+// akan bernilai true jika salah satu nilai bernilai true
+console.log(b || d || a);
+
+// And
+// akan bernilai false jika salah satu nilai bernilai false
+console.log(a && b && c);
+
+let x = 8;
+let y = 7;
+
+// Lebih Besar
+console.log(x > y);
+
+// Lebih Kecil
+console.log(x < y);
+
+// Sama Dengan
+console.log(x == y);
+
+// Lebih Besar Sama Dengan
+console.log(x >= y);
+
+// Lebih Kecil Sama Dengan
+console.log(x <= y);
+
+let lima = "5";
+let limo = 5;
+
+// Identik -  Sama Tipe Data dan Isi
+console.log(lima === limo);
